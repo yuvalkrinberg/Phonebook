6a747e34d2a94773f1e314b754941fc14dba37d0
yuvalkrin@gmail.com <yuval123654>
2020-05-10 17:21:50 +0300

    t

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const Header = (props) => {
+    return(
+        <h1>{props.course}</h1>
+    )
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Part = (props) => {
+    console.log(props);
+    return(
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Content = (props) => {
+    console.log(props);
+    const {parts} = props;
+    return(
+        <>
+            <Part part={parts[0]} />
+            <Part part={parts[1]} />
+            <Part part={parts[2]} />
+        </>
+    )
+};
+
+const Total = ({parts}) => {
+    const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+    return(
+        <p>Number of exercises {sum}</p>
+    )
+};
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
